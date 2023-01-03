d198306471dfeefe83f3ca1ae71599eb5a6ac585
LosCrimson <lukas.suslavicius@gmail.com>
2023-01-03 07:24:09 +0200

    Add project files.

---
+++ Linq_Lambda/Task1/NumberListService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Lambda.Task1
+{
+    public class NumberListService
+    {
+        public List<int> numberList = new List<int> 
+        { 1, 56, 836, 25, 45, 85};
+        
+
+    }
+}
